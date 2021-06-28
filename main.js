36ba0c50d43bb20baaa7392211dda43145f69677
AnnabelA1996 <annabelambrosius@outlook.com>
2021-06-28 13:47:21 +1200

    add markers to map

--- main.js
+++ main.js
@@ -42,3 +42,7 @@ const map = L.map('mapid', mapConfig).fitBounds(L.latLngBounds(L.latLng({
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+map.on('click', (event) => {
+  L.marker(event.latlng).addTo(map);
+});
